f40a0fe378bccf602bc038bc485b8158028b29cd
edwinaquino <github@edwinaquino.com>
2021-10-05 13:42:25 -0700

    WORKS: with routing and Build OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { Redirect, Route } from 'react-router-dom';
-import Menu from './components/Menu';
-import Page from './pages/Page';
+import Mathematics from './pages/Mathematics';
+import Home from './pages/Home';
+import Menu from "./components/Menu";
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -26,20 +27,35 @@ import './theme/variables.css';
 const App: React.FC = () => {
   return (
     <IonApp>
-      <IonReactRouter>
-        <IonSplitPane contentId="main">
-          <Menu />
-          <IonRouterOutlet id="main">
-            <Route path="/" exact={true}>
-              <Redirect to="/page/Inbox" />
-            </Route>
-            <Route path="/page/:name" exact={true}>
-              <Page />
-            </Route>
-          </IonRouterOutlet>
-        </IonSplitPane>
-      </IonReactRouter>
-    </IonApp>
+    {/* // LOCAHOST */}
+    <IonReactRouter basename={'/IONIC/React/SCHOOl/IonicMathApp/IonicMathApp1/build/'}> 
+    {/* // REMOTE */}
+    {/* <IonReactRouter basename={'/pre-school/math2/react/build/'}> */}
+
+
+    <Menu />
+      <IonRouterOutlet   id="main">
+      
+      {/* PRODUCTION */}
+      {/* https://medium.com/@svinkle/how-to-deploy-a-react-app-to-a-subdirectory-f694d46427c1 */}
+      {/* <Route path={ `${process.env.PUBLIC_URL}/`} component={Home} />
+      <Route path={`${process.env.PUBLIC_URL}/mathematics/:operator/:limit/:common/`} component={Mathematics} />
+      <Route exact path="/">  
+      <Redirect to="/home" /> */}
+      
+      {/*LOCAL APP  */}
+      
+      <Route path={`/home`} component={Home} />
+      <Route path={`/mathematics/:operator/:limit/:common`} component={Mathematics} /> 
+      <Route exact path="/">  
+      <Redirect to="/home" />
+     
+        
+          
+        </Route>
+      </IonRouterOutlet>
+    </IonReactRouter>
+  </IonApp>
   );
 };
 
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   IonContent,
   IonIcon,
@@ -7,94 +8,201 @@ import {
   IonListHeader,
   IonMenu,
   IonMenuToggle,
-  IonNote,
-} from '@ionic/react';
-
-import { useLocation } from 'react-router-dom';
-import { archiveOutline, archiveSharp, bookmarkOutline, heartOutline, heartSharp, mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, trashOutline, trashSharp, warningOutline, warningSharp } from 'ionicons/icons';
-import './Menu.css';
-
+  IonButton,
+  IonRadioGroup,
+  IonRadio,
+  IonItemDivider,
+  IonSelect,
+  IonSelectOption,
+  IonInput,
+} from "@ionic/react";
+import { useLocation } from "react-router-dom";
+import {
+  trendingUpOutline,
+  heartSharp,
+  removeCircleOutline,
+  mailSharp,
+  closeCircleOutline,
+  paperPlaneSharp,
+  addCircleOutline,
+} from "ionicons/icons";
+import "./Menu.css";
+import { Link, RouteComponentProps } from "react-router-dom";
+// DECLARE TYPES
 interface AppPage {
   url: string;
   iosIcon: string;
   mdIcon: string;
   title: string;
 }
-
+// NAVIGATIONAL LINKS
 const appPages: AppPage[] = [
   {
-    title: 'Inbox',
-    url: '/page/Inbox',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp
+    title: "Addition",
+    url: "/math/addition",
+    iosIcon: addCircleOutline,
+    mdIcon: mailSharp,
   },
   {
-    title: 'Outbox',
-    url: '/page/Outbox',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp
+    title: "Substraction",
+    url: "/math/substraction",
+    iosIcon: removeCircleOutline,
+    mdIcon: paperPlaneSharp,
   },
   {
-    title: 'Favorites',
-    url: '/page/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
+    title: "Multiplication",
+    url: "/math/multiplication",
+    iosIcon: closeCircleOutline,
+    mdIcon: heartSharp,
   },
   {
-    title: 'Archived',
-    url: '/page/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
+    title: "Division",
+    url: "/division",
+    iosIcon: trendingUpOutline,
+    mdIcon: heartSharp,
   },
-  {
-    title: 'Trash',
-    url: '/page/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/page/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
-  }
 ];
-
-const labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-
 const Menu: React.FC = () => {
-  const location = useLocation();
+  // const SendToHome = () => {
+  //     setData("This message was sent from Menu.tsx");
+  //   }
+  // const location = useLocation();
+  const [selected, setSelected] = useState<string>("Addition");
+  const [NumberLimit, setNumberLimit] = useState<number>(9);
+  const [NumberCommon, setNumberCommon] = useState<number>(2);
+  const [gender, setGender] = useState<string>();
+  const [data, setData] = useState("");
+  
+  const openUrl = (url:string) => {
+    window.open(url);
+  };
+  
+
 
   return (
-    <IonMenu contentId="main" type="overlay">
+    <IonMenu side="start" contentId="main" type="overlay">
       <IonContent>
         <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
-          {appPages.map((appPage, index) => {
-            return (
-              <IonMenuToggle key={index} autoHide={false}>
-                <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}>
-                  <IonIcon slot="start" ios={appPage.iosIcon} md={appPage.mdIcon} />
+          <IonItemDivider>Math Operations</IonItemDivider>
+          <IonRadioGroup
+            value={selected}
+            onIonChange={(e) => setSelected(e.detail.value)}
+          >
+            {appPages.map((appPage, i) => {
+              return (
+                // <IonMenuToggle key={i} autoHide={false}> // CLOSE MENU ONCLICK
+                <IonItem key={i}>
+                  {/* // <IonItem className={location.pathname === appPage.url ? 'selected' : ''} routerLink={appPage.url} routerDirection="none" lines="none" detail={false}> */}
                   <IonLabel>{appPage.title}</IonLabel>
+                  <IonRadio slot="start" value={appPage.title} />
+                  <IonIcon slot="start" icon={appPage.iosIcon} />
                 </IonItem>
-              </IonMenuToggle>
-            );
-          })}
+                // </IonMenuToggle>
+              );
+            })}
+          </IonRadioGroup>
         </IonList>
+        
+        {/* # SHOWS THE SELETECT OPTION FROM THE LIST */}
+        {/* <IonItemDivider>Your Selection</IonItemDivider>
+        <IonItem>{selected ?? "(none selected"}</IonItem>
+        <IonItemDivider>Options</IonItemDivider> */}
+        
+        
+        
+        <IonList>
+          {/* <IonItem>
+            <IonLabel>Gender</IonLabel>
+            <IonSelect value={gender} placeholder="Select One" onIonChange={e => setGender(e.detail.value)}>
+              <IonSelectOption value="female">Female</IonSelectOption>
+              <IonSelectOption value="male">Male</IonSelectOption>
+            </IonSelect>
+          </IonItem> */}
+         
+          
+          <IonItem>
+            <IonLabel>Common Number </IonLabel>
+            <IonSelect
+              interface="popover"
+              placeholder="Select Limit Number"
+              value={NumberCommon}
+              okText="OK"
+              cancelText="Cancel"
+              onIonChange={(e) => setNumberCommon(e.detail.value)}
+            >
+              <IonSelectOption value="1">1</IonSelectOption>
+              <IonSelectOption value="2">2</IonSelectOption>
+              <IonSelectOption value="3">3</IonSelectOption>
+              <IonSelectOption value="4">4</IonSelectOption>
+              <IonSelectOption value="5">5</IonSelectOption>
+              <IonSelectOption value="6">6</IonSelectOption>
+              <IonSelectOption value="7">7</IonSelectOption>
+              <IonSelectOption value="8">8</IonSelectOption>
+              <IonSelectOption value="9">9</IonSelectOption>
+              {/* {[...Array(9)].map((e, i) => {
+                                if(i===0) return false; // skip
+                                let valNumber = i + 1
+                                console.dir(valNumber);
+                                return <IonSelectOption value={valNumber}>{valNumber}</IonSelectOption>
+                            })} */}
+            </IonSelect>
+          </IonItem>
+          <IonItem>
+            <IonLabel>Limit Number</IonLabel>
+            <IonSelect
+              value={NumberLimit}
+              placeholder="Select Common Number"
+              okText="OK"
+              cancelText="Cancel"
+              onIonChange={(e) => setNumberLimit(e.detail.value)}
+            >
+              {[...Array(9)].map((e, i) => {
+                let valNumber = i + 1;
+                return (
+                  <IonSelectOption key={i} value={valNumber}>
+                    {valNumber}
+                  </IonSelectOption>
+                );
+              })}
+            </IonSelect>
+          </IonItem>
 
-        <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
-          {labels.map((label, index) => (
-            <IonItem lines="none" key={index}>
-              <IonIcon slot="start" icon={bookmarkOutline} />
-              <IonLabel>{label}</IonLabel>
-            </IonItem>
-          ))}
+          <IonItem>
+          <IonLabel>Number Of Questions</IonLabel>
+          <IonInput type="text"></IonInput>
+          </IonItem>
+         
+
+          <IonItem>
+          <IonLabel>Timer Limit</IonLabel>
+          <IonInput type="time"></IonInput>
+          </IonItem>
+
+
         </IonList>
+        <IonMenuToggle>
+          <IonButton expand="block" color="success"
+          href={
+            process.env.PUBLIC_URL +
+            "/mathematics/" +
+            selected +
+            "/" +
+            NumberLimit +
+            "/" +
+            NumberCommon
+          }
+          >
+            Done
+            
+          </IonButton>
+        </IonMenuToggle>
+        
+
+
+ 
       </IonContent>
     </IonMenu>
   );
+  
 };
-
 export default Menu;
